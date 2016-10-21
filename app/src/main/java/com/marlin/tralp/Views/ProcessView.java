af75f20f928c760bde55713e03ee0920e526e375
paulo <paulo.22.5@gmail.com>
2016-10-21 13:44:44 -0200

    ProcessView

--- app/src/main/java/com/marlin/tralp/Views/ProcessView.java
+++ app/src/main/java/com/marlin/tralp/Views/ProcessView.java
@@ -34,27 +34,7 @@ public class ProcessView extends Activity{
         View rootview = inflater.inflate(R.layout.process_view, container, false);
         rootview.setVisibility(View.VISIBLE);
         Log.d("msg ", " Entrei ProcessView.onCreateView");
-        //MainApplication app = (MainApplication) getActivity().getApplicationContext();
-
-        /* Instanciate the handler object to be used as communication source*/
-//        uiThreadHandler = new Handler(Looper.getMainLooper()) {
-//            @Override
-//            public void handleMessage(Message inputMessage) {
-//                Log.d("msg uiThreadHandler: ", " " + inputMessage.what);
-//                //String receivedObject = (String) inputMessage.obj;
-//                //String someKey = inputMessage.getData().getString("thisKey");
-//                TextView mTxtView = (TextView)findViewById(R.id.textView); // getView().
-//                mTxtView.setText("Consegui!! "+inputMessage.what);
-//                //mTxtView.setText(receivedObject);
-//                //Log.d("msg", someKey);
-//                //filterThread.interrupt();
-//                //filterThread.interrupt();
-//            }
-//        };
-//        imageProcessControllerThread = new Thread(new Controller((MainApplication)this.getApplicationContext(), uiThreadHandler));
-//        imageProcessControllerThread.start();
-
-        //Should create Thread and keep this Fragment updated
+    
         return rootview;
     }
 }
